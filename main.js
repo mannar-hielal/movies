31dce5888bd703217c2aa1d7aec7d7eab7f00e95
Mannar Hielal <mannar.hielal@liip.ch>
2022-06-07 07:50:48 +0200

    Load result to the DOM

--- main.js
+++ main.js
@@ -5,13 +5,38 @@ const SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=adf6b92471
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
+const main = document.getElementById("main");
+
 async function getMoviesFromAPI(url) {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.results);
+    showMovies(data.results);
 }
 
 getMoviesFromAPI(API_URL);
+
+function showMovies(movies) {
+    main.innerHTML="";
+
+    movies.forEach(movie=> {
+        const {title, vote_average, poster_path, overview} = movie;
+        const movieEl = document.createElement("div");
+
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
+            <img src="${IMAGE_PATH + poster_path}" alt="">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="green">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                <div>${overview}</div>
+            </div>
+        `;
+        main.appendChild(movieEl);
+    })
+}
 
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
